4eaf09cc98092e3fdbcb1f43f46e54aabe8ae0ad
Augustin Martignoni <augustin@martignoni.net>
2022-05-20 17:41:50 +0200

    File formatting using black.

--- exercise3/features/features.py
+++ exercise3/features/features.py
@@ -11,19 +11,15 @@ def get_features(image):
     image_features = []
     image = image.T
     for line in range(image.shape[0] - 1):
-        image_features.append(
-            calculate_feature_vector(image[line], image[line + 1])
-        )
+        image_features.append(calculate_feature_vector(image[line], image[line + 1]))
     return normalize(np.array(image_features).T)
 
 
 def normalize(feature_vectors):
     transposed = feature_vectors.T
-    #print(transposed, transposed.shape)
+    # print(transposed, transposed.shape)
     for column in transposed:
-        column = (
-            column - column.mean()
-        ) / column.std()
+        column = (column - column.mean()) / column.std()
     return transposed.T
 
 
@@ -33,19 +29,19 @@ def calculate_feature_vector(window, next_window):
         lower_contour(window),
         number_of_black_white_transitions(window),
         fraction_of_black_pixels(window),
-        #fraction_of_black_pixels_between_uc_and_lc(window),
+        # fraction_of_black_pixels_between_uc_and_lc(window),
         gradient_lc(window, next_window),
         gradient_uc(window, next_window),
     ]
 
 
 def upper_contour(window):
-    
+
     idx = np.flatnonzero(window)
 
     if idx.size == 0:
 
-        return window.shape[0] - 1 
+        return window.shape[0] - 1
 
     return idx[0]
 
@@ -56,10 +52,9 @@ def lower_contour(window):
 
     if idx.size == 0:
 
-        return 0 
+        return 0
 
     return idx[-1]
-
 
 
 def number_of_black_white_transitions(window):
@@ -72,7 +67,7 @@ def fraction_of_black_pixels(window):
 
 def fraction_of_black_pixels_between_uc_and_lc(window):
     return np.count_nonzero(
-        window[upper_contour(window) : lower_contour(window)] 
+        window[upper_contour(window) : lower_contour(window)]
     ) / len(window)
 
 
